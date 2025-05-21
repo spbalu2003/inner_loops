71c217f726ca4cbdf47a662030e7a080e57a8a7b
Siddamsetti Pannaga Bala Subrahmanyam <balusiddamsetti@gmail.com>
2025-05-21 23:20:20 +0530

    Update while.py

--- while.py
+++ while.py
@@ -1,3 +1,23 @@
+''' print this pattern
+"""
+1
+2
+2
+3
+4
+4
+4
+4
+5
+6
+6
+6
+6
+6
+6
+"""
+'''
+
 n=1
 while 1:          #we can take kind of true value condition for it --> example: n>0, 1*1, 2-1
     if(n%2!=0):
